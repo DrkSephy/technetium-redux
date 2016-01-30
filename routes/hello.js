7827cae2f3c0e9df309f4f8358cc06d5dd7b8748
DrkSephy <dleonar00@citymail.cuny.edu>
2016-01-30 14:53:20 -0500

    Fixing indentation

--- routes/hello.js
+++ routes/hello.js
@@ -1,5 +1,5 @@
 module.exports = (app) => {
-	app.get('/api/hello', (req, res) => {
-		res.send('Hello World!');
-	});
+  app.get('/api/hello', (req, res) => {
+    res.send('Hello World!');
+  });
 }
--- routes/issues.js
+++ routes/issues.js
@@ -1,9 +1,9 @@
 // Base url for issues: https://bitbucket.org/api/1.0/repositories/{username}/{reponame}/issues
 module.exports = (app, request) => {
-	app.get('/api/issues', (req, res) => {
-		request.get('https://bitbucket.org/api/1.0/repositories/DrkSephy/technetium-redux-testing/issues/', 
-			(error, response, body) => {
-				res.send(body);	
-			});
-	});
+  app.get('/api/issues', (req, res) => {
+    request.get('https://bitbucket.org/api/1.0/repositories/DrkSephy/technetium-redux-testing/issues/', 
+      (error, response, body) => {
+        res.send(body); 
+      });
+  });
 }
