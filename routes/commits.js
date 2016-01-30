274621445016e5be3007c39ec8d7f2b07fa4a257
DrkSephy <dleonar00@citymail.cuny.edu>
2016-01-30 18:25:27 -0500

    Using promises for returning number of commits

--- routes/commits.js
+++ routes/commits.js
@@ -9,20 +9,12 @@ module.exports = (app, request) => {
   });
 
   app.get('/api/commits', (req, res) => {
-    request.get('https://bitbucket.org/api/1.0/repositories/DrkSephy/technetium-redux-testing/changesets', 
-      (error, response, body) => {
-        var data = JSON.parse(body);
-        console.log(data.count);
-        res.json(data.changesets);
-      });
+    var count = new Promise((resolve, reject) => {
+      request.get('https://bitbucket.org/api/1.0/repositories/DrkSephy/technetium-redux-testing/changesets', 
+        (error, response, body) => {
+          var data = JSON.parse(body);
+          resolve(data.count);
+        });
+    }).then((data) => res.json(data));
   });
-
-  function computeQueries() {
-    var count;
-    request.get('https://bitbucket.org/api/1.0/repositories/DrkSephy/technetium-redux-testing/changesets/', 
-      (error, response, body) => {
-        console.log('hello');
-        console.log(body.changesets);
-    });
-  }
 }
