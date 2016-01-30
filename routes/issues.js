1e897c60152ccb8cd0dbdc74d9ef010ce6fc6eb2
DrkSephy <dleonar00@citymail.cuny.edu>
2016-01-29 23:12:49 -0500

    Documenting issues route

--- routes/issues.js
+++ routes/issues.js
@@ -1,6 +1,7 @@
+// Base url for issues: https://bitbucket.org/api/1.0/repositories/{username}/{reponame}/issues
 module.exports = (app, request) => {
 	app.get('/api/issues', (req, res) => {
-		request.get('https://bitbucket.org/api/1.0/repositories/DrkSephy/technetium-redux-testing/issues/?sort=kind', 
+		request.get('https://bitbucket.org/api/1.0/repositories/DrkSephy/technetium-redux-testing/issues/', 
 			(error, response, body) => {
 				res.send(body);	
 			});
