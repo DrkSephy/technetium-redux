fecd3959fee7ac70a5642257ea3b15f438d750b7
DrkSephy <dleonar00@citymail.cuny.edu>
2016-01-30 15:40:34 -0500

    Fixing API route for count.js

--- routes/count.js
+++ routes/count.js
@@ -1,5 +1,5 @@
 module.exports = (app, request) => {
-  app.get('/api/commits', (req, res) => {
+  app.get('/api/count', (req, res) => {
     request.get('https://bitbucket.org/api/1.0/repositories/DrkSephy/technetium-redux-testing/changesets/', (error, response, body) => {
       res.send(body);
     })
