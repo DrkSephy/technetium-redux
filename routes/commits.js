15ae74792ec2d0fac70fd0b8dcb78f43c05185f1
DrkSephy <dleonar00@citymail.cuny.edu>
2016-01-30 18:16:20 -0500

    Testing functions for getting total commit count

--- routes/commits.js
+++ routes/commits.js
@@ -10,8 +10,19 @@ module.exports = (app, request) => {
 
   app.get('/api/commits', (req, res) => {
     request.get('https://bitbucket.org/api/1.0/repositories/DrkSephy/technetium-redux-testing/changesets', 
-      (error, respond, body) => {
-        res.send(body);
+      (error, response, body) => {
+        var data = JSON.parse(body);
+        console.log(data.count);
+        res.json(data.changesets);
       });
-  })
+  });
+
+  function computeQueries() {
+    var count;
+    request.get('https://bitbucket.org/api/1.0/repositories/DrkSephy/technetium-redux-testing/changesets/', 
+      (error, response, body) => {
+        console.log('hello');
+        console.log(body.changesets);
+    });
+  }
 }
