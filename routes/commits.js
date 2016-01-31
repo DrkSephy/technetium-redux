7ebe7badf0e866e62ad724c6cbcb98afb3298b49
DrkSephy <dleonar00@citymail.cuny.edu>
2016-01-30 23:14:58 -0500

    Code cleanup

--- routes/commits.js
+++ routes/commits.js
@@ -11,37 +11,33 @@ module.exports = (app, request, _) => {
   });
 
   app.get('/api/commits', (req, res) => {
-    // Get number of commits in the repository
     getJSON('https://bitbucket.org/api/1.0/repositories/DrkSephy/wombat/changesets')
-    // Compute all urls
     .then((data) => {
+
       let promises = computeUrls(data.count);
 
       Promise.all(promises)
-        .then((results) => {
-          let users = []
-          let parsedData = [];
-          let commitData = {}
-          results.forEach((item) => {
-            const data = item.values;
-            for (let value in data) {
-              const username = data[value].author.user.username;
-
-              // Check if we created a data object for a contributor
-              if(username !== undefined && !(_.contains(users, username))) {
-                users.push(username);
-                commitData[username] = {
-                  commits: 0
-                };
-              }
-
-              commitData[username].commits += 1;
+      .then((results) => {
+        let users = []
+        let parsedData = [];
+        let commitData = {}
+        results.forEach((item) => {
+          const data = item.values;
+          for (let value in data) {
+            const username = data[value].author.user.username;
+            if(username !== undefined && !(_.contains(users, username))) {
+              users.push(username);
+              commitData[username] = {
+                commits: 0
+              };
             }
-          });
-
-          res.json(commitData);
-
+            commitData[username].commits += 1;
+          }
         });
+
+        res.json(commitData);
+
+      });
     });
 
   });
@@ -56,7 +52,6 @@ module.exports = (app, request, _) => {
   }
 
   function computeUrls(count) {
-    console.log(count);
     let urls = [];
     let page = 1;
     let stop = Math.floor(count / 30);
