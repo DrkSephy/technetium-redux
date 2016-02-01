14d6c30eaf45b2dab1abfc21705e798a1f22dee8
DrkSephy <dleonar00@citymail.cuny.edu>
2016-02-01 10:43:48 -0500

    Updating route handlers and App

--- app/components/App.js
+++ app/components/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Commits from './Commits';
+import Navbar from './Navbar';
 
 class App extends React.Component {
   render() {
     return (
       <div>
+        <Navbar history={this.props.history} />
         {this.props.children}
       </div>
     );
--- app/routes.js
+++ app/routes.js
@@ -2,9 +2,11 @@ import React from 'react';
 import {Route} from 'react-router';
 import App from './components/App';
 import Home from './components/Home';
+import Commits from './components/Commits';
 
 export default (
   <Route component={App}>
     <Route path='/' component={Home} />
+    <Route path='/commits' component={Commits} />
   </Route>
 );
