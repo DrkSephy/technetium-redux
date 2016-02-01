1bbb1f3e88c1a04bae595bc42211c4bf34fa9504
DrkSephy <dleonar00@citymail.cuny.edu>
2016-01-31 22:02:56 -0500

    Adding basic components and doing server-side rendering

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
+// Babel ES6/JSX Compiler
+require('babel-register');
+
+var swig = require('swig');
+var React = require('react');
+var ReactDOM = require('react-dom/server');
+var Router = require('react-router');
+var routes = require('./app/routes');
+
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
@@ -17,6 +26,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 require('./routes/issues')(app, request);
 require('./routes/commits')(app, request, _, config);
+
+app.use(function(req, res) {
+  Router.match({ routes: routes.default, location: req.url }, function(err, redirectLocation, renderProps) {
+    if (err) {
+      res.status(500).send(err.message)
+    } else if (redirectLocation) {
+      res.status(302).redirect(redirectLocation.pathname + redirectLocation.search)
+    } else if (renderProps) {
+      var html = ReactDOM.renderToString(React.createElement(Router.RoutingContext, renderProps));
+      var page = swig.renderFile('views/index.html', { html: html });
+      res.status(200).send(page);
+    } else {
+      res.status(404).send('Page Not Found')
+    }
+  });
+});
 
 app.listen(app.get('port'), () => 
   console.log('Express server listening on port ' + app.get('port')));
