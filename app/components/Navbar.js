5c1434d42a72fdd62cb1d0349656f61d577323c0
DrkSephy <dleonar00@citymail.cuny.edu>
2016-02-01 10:42:36 -0500

    Adding Navbar component

---
+++ app/components/Navbar.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onChange = this.onChange.bind(this);
+  }
+
+  componentDidMount() {
+    // NavbarStore.listen(this.onChange);
+  }
+
+  componentWillUnmount() {
+    // NavbarStore.unlisten(this.onChange);
+  }
+
+  onChange(state) {
+    this.setState(state);
+  }
+
+  render() {
+    return (
+      <nav className='navbar navbar-default navbar-static-top'>
+        <div className='navbar-header'>
+          <button type='button' className='navbar-toggle collapsed' data-toggle='collapse' data-target='#navbar'>
+            <span className='sr-only'>Toggle navigation</span>
+            <span className='icon-bar'></span>
+            <span className='icon-bar'></span>
+            <span className='icon-bar'></span>
+          </button>
+        </div>
+        <div id='navbar' className='navbar-collapse collapse'>
+          <ul className='nav navbar-nav'>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/commits'>Commits</Link></li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
