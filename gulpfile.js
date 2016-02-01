def7825f99bda57047d25630de75d8a2921c1aab
DrkSephy <dleonar00@citymail.cuny.edu>
2016-01-31 22:11:27 -0500

    Adding styles tasks into gulpfile, updating index.html

--- gulpfile.js
+++ gulpfile.js
@@ -78,5 +78,19 @@ gulp.task('browserify-watch', ['browserify-vendor'], function() {
   }
 });
 
+// compile LESS stylesheets
+gulp.task('styles', function() {
+  return gulp.src('app/stylesheets/main.less')
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(autoprefixer())
+    .pipe(gulpif(production, cssmin()))
+    .pipe(gulp.dest('public/css'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('app/stylesheets/**/*.less', ['styles']);
+});
+
 gulp.task('default', ['styles', 'vendor', 'browserify-watch', 'watch']);
 gulp.task('build', ['styles', 'vendor', 'browserify']);
