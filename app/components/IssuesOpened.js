9fd858e568bfd97a73522d35e85982d7e324a082
DrkSephy <dleonar00@citymail.cuny.edu>
2016-02-04 18:35:56 -0500

    Implemented IssuesOpened Component

---
+++ app/components/IssuesOpened.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import IssuesOpenedStore from '../stores/IssuesOpenedStore';
+import IssuesOpenedActions from '../actions/IssuesOpenedActions';
+
+class Issues extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = IssuesOpenedStore.getState();
+    this.onChange = this.onChange.bind(this);
+  }
+
+  componentDidMount() {
+    IssuesOpenedStore.listen(this.onChange);
+    IssuesOpenedActions.getOpenedIssues();
+  }
+
+  componentWillUnmount() {
+    IssuesOpenedStore.unlisten(this.onChange);
+  }
+
+  onChange(state) {
+    this.setState(state);
+  }
+
+  render() {
+    let issuesOpened = this.state.issuesOpened.map((data) => {
+      return (
+        <tbody>
+          <tr>
+            <td>{data.username}</td>
+            <td>{data.opened}</td>
+          </tr>
+        </tbody>
+      );
+
+      return (
+        <div className='container'>
+          <div className='panel panel-default'>
+            <table className='table table-striped'>
+              <thead>
+              <tr>
+                <td colSpan='1'>Username</td>
+                <td colSpan='1'>Opened</td>
+              </tr>
+              </thead>
+                {issuesOpened}
+            </table>
+          </div>
+        </div>
+      );
+    });
+  }
+}
+
+export default Issues;
