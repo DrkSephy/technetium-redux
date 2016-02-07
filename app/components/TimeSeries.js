ff3d48de417804c746b7c9162ca0e569cc7b2c80
DrkSephy <dleonar00@citymail.cuny.edu>
2016-02-06 22:57:13 -0500

    Implemented Time Series Component

---
+++ app/components/TimeSeries.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {Link} from 'react-router';
+import TimeSeriesStore from '../stores/TimeSeriesStore';
+import TimeSeriesActions from '../actions/TimeSeriesActions';
+
+class TimeSeries extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = TimeSeriesStore.getState();
+    this.onChange = this.onChange.bind(this);
+  }
+
+  componentDidMount() {
+    TimeSeriesStore.listen(this.onChange);
+    TimeSeriesActions.getData();
+  }
+
+  componentWillUnmount() {
+    TimeSeriesStore.unlisten(this.onChange);
+  }
+
+  onChange(state) {
+    this.setState(state);
+  }
+
+  render() {
+    return (
+      <div className='chart_2'></div>
+    );
+  } 
+} 
+
+export default TimeSeries;
