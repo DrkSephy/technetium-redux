521b7c51dcdd757e8afa0f2053cf83773bcba863
DrkSephy <dleonar00@citymail.cuny.edu>
2016-02-06 22:02:23 -0500

    Implemented basic line chart API route

---
+++ routes/charts.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = (app) => {
+  app.get('/api/charts', (req, res) => {
+    let data = {
+      columns: [
+        ['data1', 30, 200, 100, 400, 150, 250],
+        ['data2', 50, 20, 10, 40, 15, 25]
+      ]
+    }
+    res.send(data.columns);
+  });
+}
