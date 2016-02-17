f547c360d7b316987ace2bb2bafbc49be1a79b05
DrkSephy <dleonar00@citymail.cuny.edu>
2016-02-16 19:46:52 -0500

    Adding Panels to Reports Component

--- app/components/Reports.js
+++ app/components/Reports.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router';
+import {Panel} from 'react-bootstrap';
 import TimeSeries from './TimeSeries';
 import ReportsStore from '../stores/ReportsStore';
 import ReportsActions from '../actions/ReportsActions';
@@ -25,6 +26,7 @@ class Reports extends React.Component {
   }
 
   render() {
+
     let reportData = this.state.reportData.map((data) => {
       return (
         <tr key={data.id}>
@@ -38,30 +40,44 @@ class Reports extends React.Component {
         </tr>
       );
     });
-    
+
     return (
-      <div className='container'>
-        <div className='panel panel-default'>
-          <table className='table table-striped'>
-            <thead>
-            <tr>
-              <th colSpan='1'>Username</th>
-              <th colSpan='1'>Commits</th>
-              <th colSpan='1'>Pull Requests</th>
-              <th colSpan='1'>Issues Assigned</th>
-              <th colSpan='1'>Issues Closed</th>
-              <th colSpan='1'>Issues Opened</th>
-              <th colSpan='1'>Issue Comments</th>
-            </tr>
-            </thead>
-            <tbody>
-              {reportData}
-            </tbody>
-          </table>
+      <div>
+        <div>
+          <Panel header='Repository Statistics'>
+            <div className='container'>
+              <div className='panel panel-default'>
+                <table className='table table-striped'>
+                  <thead>
+                  <tr>
+                    <th colSpan='1'>Username</th>
+                    <th colSpan='1'>Commits</th>
+                    <th colSpan='1'>Pull Requests</th>
+                    <th colSpan='1'>Issues Assigned</th>
+                    <th colSpan='1'>Issues Closed</th>
+                    <th colSpan='1'>Issues Opened</th>
+                    <th colSpan='1'>Issue Comments</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                    {reportData}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </Panel>
         </div>
-        <TimeSeries />
+
+        <div>
+          <Panel header='Commit Time Series'>
+            <div className='container'>
+              <TimeSeries />
+            </div>
+          </Panel>
+        </div>
       </div>
-    );    
+
+    );  
   }
 }
 
