93e4acde6c1fb88a0045c7da4ab26253eb14e69f
DrkSephy <dleonar00@citymail.cuny.edu>
2016-02-18 18:40:08 -0500

    Implemented Subscriptions Actions

---
+++ app/actions/SubscriptionsActions.js
@@ -0,0 +1,28 @@
+import alt from '../alt';
+
+class SubscriptionsActions {
+  constructor() {
+    this.generateActions(
+      'addSubscriptionSuccess',
+      'addSubscriptionFail',
+      'updateUrl',
+      'invalidUrl'
+    );
+  }
+
+  addSubscription(url) {
+    $.ajax({ 
+      type: 'POST',
+      url: '/api/subscriptions',
+      data: { url: url }
+    })
+    .done((data) => {
+      this.actions.addSubscriptionSuccess(data.message);
+    })
+    .fail((jqXhr) => {
+      this.actions.addSubscriptionFail(jqXhr.responseJSON.message);
+    });
+  }
+}
+
+export default alt.createActions(SubscriptionsActions);
