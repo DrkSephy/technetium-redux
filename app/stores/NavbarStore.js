37ac8e3bc7e52d5557b8eeff393d1907b9d34966
DrkSephy <dleonar00@citymail.cuny.edu>
2016-02-18 22:59:25 -0500

    Implemented Navbar Store for storing subscriptions data

---
+++ app/stores/NavbarStore.js
@@ -0,0 +1,19 @@
+import alt from '../alt';
+import NavbarActions from '../actions/NavbarActions';
+
+class NavbarStore {
+  constructor() {
+    this.bindActions(NavbarActions);
+    this.subscriptions = [];
+  }
+
+  onGetSubscriptionsSuccess(data) {
+    this.subscriptions = data;
+  }
+
+  onGetSubscriptionsFail(jqXhr) {
+    toastr.error(jqXhr.responseJSON && jqXhr.responseJSON.message || jqXhr.responseText || jqXhr.statusText);
+  }
+}
+
+export default alt.createStore(NavbarStore);
