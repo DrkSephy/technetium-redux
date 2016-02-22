619744b8dda5115be1dc17773a285b3b7c920e72
DrkSephy <dleonar00@citymail.cuny.edu>
2016-02-22 00:05:20 -0500

    Resolved #32: Implemented Tooltip Component

---
+++ app/components/LinkWithTooltip.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Tooltip, OverlayTrigger} from 'react-bootstrap';
+
+class LinkWithTooltip extends React.Component {
+  render() {
+    let tooltip = <Tooltip>{this.props.tooltip}</Tooltip>;
+
+    return (
+      <OverlayTrigger
+        overlay={tooltip} placement="left"
+        delayShow={200} delayHide={150}
+      >
+        <a href={this.props.href}>{this.props.children}</a>
+      </OverlayTrigger>
+    );
+  }
+}
+
+export default LinkWithTooltip;
