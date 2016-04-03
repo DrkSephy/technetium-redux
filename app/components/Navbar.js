cb77e77163fbf73ae399699d03c5dbb46ee0249a
DrkSephy <dleonar00@citymail.cuny.edu>
2016-04-03 09:04:10 -0400

    Adding Glyphicons

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router';
+import {Glyphicon} from 'react-bootstrap';
 import Login from '../components/Login';
 import LoginStore from '../stores/LoginStore';
 import NavbarStore from '../stores/NavbarStore';
@@ -55,18 +56,18 @@ class Navbar extends React.Component {
               <li><Link to='/'>Home</Link></li>
               <li><Link to='/subscriptions'>Subscribe</Link></li>
               <li className='dropdown'>
-                <a href='#' className='dropdown-toggle' data-toggle='dropdown'>Subscriptions<span className='caret'></span></a>
+                <a href='#' className='dropdown-toggle' data-toggle='dropdown'>Reports<span className='caret'></span></a>
                 <ul className='dropdown-menu'>
                   {subscriptions}
                 </ul>
               </li>
             </ul>
             <ul className='nav navbar-nav navbar-right' style={navStyle}>
+              <li><a href='#' onClick={NavbarActions.removeSubscriptions}>Unsubscribe From All</a></li>
               <li className='dropdown'>
-                <a href='#' className='dropdown-toggle' data-toggle='dropdown'>Hello, {LoginStore.getState().username}<b className='caret'></b></a>
+                <a href='#' className='dropdown-toggle' data-toggle='dropdown'><span><Glyphicon glyph='user' /></span> Hello, {LoginStore.getState().username}<b className='caret'></b></a>
                 <ul className='dropdown-menu'>
                   <li><a href='/logout'>Logout</a></li>
-                  <li><a href='#' onClick={NavbarActions.removeSubscriptions}>Unsubscribe</a></li>
                 </ul>
               </li>
             </ul>
