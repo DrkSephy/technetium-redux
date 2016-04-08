53374ab069f4ff421d54aa079a353a196b0ed50a
DrkSephy <dleonar00@citymail.cuny.edu>
2016-04-07 20:38:49 -0400

    Cleaning up Footer component

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -9,12 +9,10 @@ class Footer extends React.Component {
     return (
       <footer>
         <div className='container'>
-          <div className='row'>
-            <div className='col-sm-5'>
+          <div className='row text-center'>
+            <div>
               <h3 className='lead'></h3>
-              <p>Powered by <strong>Node.js</strong>,  <strong>Express.js</strong>, <strong>MongoDB</strong> and <strong>React</strong> with <strong>Flux</strong> architecture.</p>
-              <p>You may view the <a href='https://github.com/DrkSephy/technetium-redux'>Source Code</a> behind this project on GitHub.</p>
-              <p>© 2016 <a href='https://github.com/DrkSephy'>David Leonard.</a></p>
+              <p>Powered by Node, Express, React and Flux. <a href='https://github.com/DrkSephy/technetium-redux'>Source Code</a> © 2016 <a href='https://github.com/DrkSephy'>David Leonard</a>.</p>
             </div>
           </div>
         </div>
