b270865018b0dd40474d9277a57b3ded09fd3b0c
DrkSephy <dleonar00@citymail.cuny.edu>
2016-04-10 22:07:39 -0400

    Implementing api route for getting user subscriptions

---
+++ routes/repositories.js
@@ -0,0 +1,50 @@
+/**
+ * Retrives user's repositories.
+ * @module routes/repositories
+*/
+
+var User = require('../models/users');
+import {getJSON, computeRepositoryUrls, isAuthenticated, generateRandomNumber} from './utils';
+
+'use strict';
+
+module.exports = (app) => {
+  app.get('/api/users/repositories', isAuthenticated, (req, res, next) => {
+    User.findOne({ username: req.user.username }, (err, user) => {
+      if (err) return next(err);
+
+      if (user) {
+        console.log(user.subscriptions);
+        getJSON('https://api.bitbucket.org/2.0/repositories/' + req.user.username, req.user.authToken)
+        .then((results) => {
+          let repos = [];
+          let promises = computeRepositoryUrls('https://api.bitbucket.org/2.0/', results.size, req.user.authToken, req.user.username);
+          Promise.all(promises)
+          .then((repositories) => {
+            repositories.forEach((repository) => {
+              repository['values'].forEach((entry) => {
+                if (entry.scm == 'git') {
+                  let data = {}
+                  data.subscribed = false;
+                  data.name = entry.name;
+                  data.username = entry.owner.username;
+                  data.link = entry.links.html.href;
+                  data.id = generateRandomNumber();
+                  data.repoid = null;
+                  user.subscriptions.forEach((subscription) => {
+                    if (subscription.reponame == entry.name) {
+                      data.subscribed = true;
+                      data.repoid = subscription._id;
+                    } 
+                  });
+                  repos.push(data);
+                }
+              });
+            });
+            res.send(repos);
+          });
+        });
+      }
+    });
+  });
+}
